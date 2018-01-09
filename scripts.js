bea73d609cf300fb02d7ff423934c835c3ab55e8
Sandra Banaszczyk <hejnowicz.s@gmail.com>
2018-01-09 21:40:45 +0100

    Fix return

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 function getTriangleArea(a, h) {
 	if ( (a > 0) && (h > 0) ) {
 		return a * h / 2;
-		return 'Nieprawidłowe dane';
 	}
+		return 'Nieprawidłowe dane';
 }
+
 console.log(getTriangleArea(10, 6));
 var triangle1Area = getTriangleArea(8, 3),
 	triangle2Area = getTriangleArea(7, 9),
